cec2bbebaf1c89aeb53a3eff8e788716fa11b2f3
GabrielLima2803 <gabriellima2803@gmail.com>
2024-08-20 14:43:33 -0300

    feat: Criação das funçoes  de reset e forgot password

--- core/auth/forgot_password.py
+++ core/auth/forgot_password.py
@@ -8,6 +8,9 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+@api_view(["POST"])
+@authentication_classes([])
+@permission_classes([AllowAny])
 def ForgotPasswordUser(request):
     email = request.data.get("email")
 
