4d60344e5ee1d386b6869ed7821d222382da3c9c
GabrielLima2803 <gabriellima2803@gmail.com>
2024-10-08 16:01:15 -0300

    feat: Adicionando permissão para a rota de SendMessage

--- chat/views.py
+++ chat/views.py
@@ -1,23 +1,26 @@
-from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 import json
 from .redis_client import redis_client
+from rest_framework.permissions import IsAuthenticated
 
 class SendMessageView(APIView):
+    permission_classes = [IsAuthenticated]
+
     def post(self, request):
-        user_id = request.data.get('user_id')
+        user_id = request.data.get('user_id') 
         message = request.data.get('message')
 
         if not user_id or not message:
             return Response({'detail': 'user_id e message são obrigatórios.'}, status=status.HTTP_400_BAD_REQUEST)
 
         data = {
-            'user_id': user_id,
+            'from_user_id': request.user.id,  
+            'to_user_id': user_id,
             'message': message
         }
-        
+
         try:
             redis_client.publish('chat_messages', json.dumps(data))
             return Response({'detail': 'Mensagem enviada com sucesso!'}, status=status.HTTP_200_OK)
