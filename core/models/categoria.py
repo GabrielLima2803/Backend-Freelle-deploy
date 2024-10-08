eee93a9c93bc96045b1d020fc7470d472ddc0de9
GabrielLima2803 <gabriellima2803@gmail.com>
2024-10-08 16:20:17 -0300

    feat: Adicionado o campo icon

--- core/models/categoria.py
+++ core/models/categoria.py
@@ -2,9 +2,10 @@ from django.db import models
 
 class Categoria(models.Model):
     nome = models.CharField(max_length=100, null=False, unique=True)
+    icon = models.CharField(max_length=100, null=False, blank=True)
 
     def __str__(self):
-        return f"Categoria - ${self.nome}"
+        return f"Categoria - {self.nome}"
     
     class Meta:
         verbose_name = "Categoria"
